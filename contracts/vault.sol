b0ef3d912796d6f5b48d543401f49ea58b3c1747
521047mf <78964350+521047@users.noreply.github.com>
2023-06-20 17:25:55 +0200

    Update vault.sol (add comments)

--- contracts/vault.sol
+++ contracts/vault.sol
@@ -11,19 +11,23 @@ contract Vault is ERC20, Ownable {
         address contractAddress;
         uint256 price;
     }
-
+    // create mappings
     mapping(address => mapping(uint256 => NFT)) private _depositedNFTs;
     mapping(address => bool) private _depositors;
     bool public isVaultLocked = false;
     mapping(address => uint256) private _allocations;
 
+    // constructor for vault token
     constructor() ERC20("Vault Token", "VT") Ownable() {
     }
+
+    // main functions from here 
 
     function addDepositor(address _depositor) external onlyOwner {
         _depositors[_depositor] = true;
     }
 
+    // depositNFT checks for whitelist and if the vault is locked
     function depositNFT(address _contract, uint256 _tokenId) external {
         require(_depositors[msg.sender], "Depositor not whitelisted");
         require(!isVaultLocked, "Vault is locked");
@@ -35,6 +39,7 @@ contract Vault is ERC20, Ownable {
         _depositedNFTs[_contract][_tokenId].price = _price;
     }
 
+    // check if whitelisted, and check if the vault is locked
     function withdrawNFT(address _contract, uint256 _tokenId) external {
         require(_depositedNFTs[_contract][_tokenId].owner == msg.sender, "Caller not owner");
         require(!isVaultLocked, "Vault is locked");
@@ -42,6 +47,7 @@ contract Vault is ERC20, Ownable {
         delete _depositedNFTs[_contract][_tokenId];
     }
 
+    // check if the vault is locked, and if there is enough 
     function purchaseNFT(address _contract, uint256 _tokenId) external {
         require(isVaultLocked, "Vault is not locked");
         uint256 price = _depositedNFTs[_contract][_tokenId].price;
@@ -51,22 +57,27 @@ contract Vault is ERC20, Ownable {
         delete _depositedNFTs[_contract][_tokenId];
     }
 
+    // only owner
     function lockVault() external onlyOwner {
         isVaultLocked = true;
     }
 
+    // only owner
     function unlockVault() external onlyOwner {
         isVaultLocked = false;
     }
 
+    // only owner
     function mintVaultToken(uint256 amount) external onlyOwner {
         _mint(address(this), amount);
     }
 
+    // only owner
     function setAllocation(address depositor, uint256 percentage) external onlyOwner {
         _allocations[depositor] = percentage;
     }
 
+    // only those will allocations can withdraw any tokens
     function withdrawVaultToken() external {
         uint256 balance = balanceOf(address(this));
         uint256 amount = (balance * _allocations[msg.sender]) / 100;
